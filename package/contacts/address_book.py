3cc51ecfdedee41b0b54b653bdc32bc627f02636
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-09-08 01:21:03 +0300

    start struct

---
+++ package/contacts/address_book.py
@@ -0,0 +1,395 @@
+from collections import UserDict
+import json
+from datetime import date 
+import typing as t
+
+class Field:
+    """
+    Class parent representing a field used in the record of the address book.
+    """
+    def __init__(self, value: str) -> None:
+        self.value = value 
+      
+    def __valid_value(self, value) -> None:
+        if not isinstance(value, str):
+            raise TypeError(f'Value {value} is not valid. Must be string')
+        
+    @property
+    def value(self) :
+        return self._value
+    
+    @value.setter
+    def value(self, value: str, validation: t.Callable | None = None) -> None:
+        self.__valid_value(value)
+        if validation is not None:
+            value = validation(value)
+        self._value = value
+
+    def __str__(self) -> str:
+        return f'{self.value}'
+    
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(value={self.value})'
+    
+    def __eq__(self, val):  # ==
+        if isinstance(val, self.__class__): # можно и через if hasattr(val, 'value'):
+            val = val.value
+        return self.value == val
+    
+
+class Name(Field):
+    """
+    Class representing the name field in a record of  the address book.
+    """
+    pass
+ 
+       
+class Phone(Field):
+    """
+    Class representing the phone field in a record of the address book.
+    """ 
+    def __valid_phone(self, value) -> str: 
+        phone = ''.join(filter(str.isdigit, value))
+        if 9 >= len(phone) <= 15 : #псевдо проверка номера
+            raise ValueError(f"Phone number {value} isn't correct")
+        return phone
+
+    @Field.value.setter # переопределяем сеттер родительского класса
+    def value(self, value: str) -> None:
+        Field.value.fset(self, value, self.__valid_phone)
+        
+
+class Birthday(Field):
+    """
+    Class representing the birthday field in a record of the address book.
+    The date is stored in ISO 8601 format.
+    """
+
+    def __valid_date(self, value: str) -> str:
+        """
+        Input date string to a valid ISO-formatted date.
+        Args:
+            value (str): The input date string.
+        Raises:
+            ValueError: If the input date string is not in a valid date format(ISO).
+        Returns:
+            str: The valid ISO-formatted date string.    
+        """
+        try:
+            date.fromisoformat(value)
+        except ValueError: 
+            raise ValueError(f'Value {value} is not correct format! Also "2023-12-30"')
+        return value    
+    
+    @Field.value.setter
+    def value(self, value: str) -> None:
+        Field.value.fset(self, value, self.__valid_date)
+        
+    def get_date(self) -> date:
+        return date.fromisoformat(self.value)    
+
+class Record:
+    """
+    Class representing a record in an address book.
+
+    Attributes:
+        name (Name): The name of the contact.
+        phones (list): A list of phone numbers associated with the contact.
+        birthday (Birthday): The birthday of the contact.
+    """
+  
+    def __init__(
+            self, 
+            name: Name | str, 
+            phones: list[Phone] | list[str] = [], 
+            birthday: Birthday | str | None = None, 
+        ) -> None:
+
+        self.name = self._name(name)
+        self.phones = [self._phone(phone) for phone in phones]
+        self.birthday = None if birthday is None else self._birthday(birthday)
+        
+
+    def _name(self, name: str | Name) -> Name:
+        if not isinstance(name, Name):
+            name = Name(name)
+        return name
+
+    def _phone(self, phone: str | Phone) -> Phone:
+        if not isinstance(phone, Phone):
+            phone = Phone(phone)
+        return phone
+
+    def _birthday(self, birthday: str | Birthday) -> Birthday:
+        if not isinstance(birthday, Birthday):
+            birthday = Birthday(birthday)
+        return birthday    
+    
+    def add_phone(self, phone: Phone | str) -> None:
+        """
+        Add a new phone number to the list of phone numbers for the contact.
+        Args:
+            phone (Phone) or try valid Str: The phone number to be added to the contact.
+        Returns:
+            None: This method does not return any value.
+        """
+        if phone in self.phones:
+            raise ValueError("this phone number has already been added")
+        
+        phone = self._phone(phone)
+        self.phones.append(phone)
+
+    def remove_phone(self, phone: Phone | str) -> None:
+        """
+        Remove a phone number from the list of phone numbers for the contact.
+
+        Args:
+            phone (Phone) or try valid Str: The phone number to be removed from the contact.
+        Raises:
+            ValueError: If the phone number is not found in the contact's list of phone numbers.
+        Returns: 
+            None: This method does not return any value.
+        """
+        phone = self._phone(phone) # єту строку может после райза?
+        if phone not in self.phones:
+            raise ValueError(f"The phone '{phone}' is not in this record.")
+        self.phones.remove(phone)
+        
+    def change_phone(self, old_phone: Phone | str, new_phone: Phone | str) -> None:
+        """
+        Change a phone number in the list of phone numbers for the contact.
+
+        Args:
+            old_phone (Phone)  or try valid Str: The existing phone number to be replaced.
+            new_phone (Phone)  or try valid Str: The new phone number to replace the existing one.
+        Raises:
+            ValueError: If the old phone number is not found in the contact's list of phone numbers.
+            ValueError: If the new phone number is already in contact's list of phone numbers.
+        """
+        
+        if (old_phone := self._phone(old_phone)) not in self.phones: 
+            raise ValueError(f"The phone '{old_phone}' is not in this record '{self.name}'.")
+        if (new_phone := self._phone(new_phone)) in self.phones:
+            raise ValueError(f"The phone '{new_phone}' already in record '{self.name}'.")
+        inx = self.phones.index(old_phone)
+        self.phones[inx] = new_phone
+        
+    def change_birthday(self, birthday):
+        self.birthday = self._birthday(birthday)   
+
+    def days_to_birthday(self) -> int :
+        """
+        Calculate the number of days remaining until the contact's next birthday.
+
+        Returns:
+            int: The number of days remaining until the contact's next birthday.
+        Raises:
+            KeyError: If the contact does not have a birthday set.
+        """
+        if self.birthday == None:
+            raise KeyError(f"No birthday set for the contact {self.name}.")
+        
+        today = date.today()
+        try:
+            bday = self.birthday.get_date().replace(year=today.year) # дата др в этом году 
+            if today > bday : # если др уже прошло берем дату следующего(в следующем году)
+                bday= bday.replace(year=today.year+1)
+            return (bday - today).days
+        except ValueError: # исключение для високосной дати 1го дня уууу-02-29
+            exept_temp = Record(self.name, [] , today.replace(month=2, day=28).isoformat()) 
+            return exept_temp.days_to_birthday() + 1
+        
+    
+    def __str__(self) -> str:
+        # вывод телефонов с новой строки и табуляцией
+        birthday_str = f'birthday: {self.birthday or "Empty"}'
+        phones_str = ", ".join([str(ph) for ph in self.phones])
+        return (
+            f'<Record>:\n\tname: {self.name}'
+            f'\n\tphones: {phones_str or "Empty"}\n\t'
+            f'{birthday_str}\n'
+        )
+
+    def __repr__(self) -> str:
+        # __repr__ используется для того что бы показать как создается екземпляр
+        # т.е. если выполнить эту строку в repl python будет создан такой же екземпляр,
+        # конечно, при условии , что все необходимые классы тоже импортированы
+        # repl python
+        # (встроенная в пайтон среда выполенния, которую можно вызвать просто выполнив команду python)
+        return (
+            f'Record(name={self.name!r}, '
+            f'phones=[{", ".join([ph.__repr__() for ph in self.phones])}, '
+            f'birthday={self.birthday!r})'
+        )
+
+    def to_dict(self) -> dict[str, dict[str, list[str] | str | None]]:
+        phones = [str(phone) for phone in self.phones]
+        birthday = None if self.birthday is None else str(self.birthday)
+        return {
+            str(self.name): {
+                "phones": phones,
+                "birthday": birthday,
+            },
+        }
+          
+class AddressBook(UserDict):
+    """
+    A class representing an address book, which is a dictionary 
+    with record names as keys and record objects as values.
+    """
+    
+    def add_record(self, record: Record) -> None:
+        """
+        Add a record to the address book.
+
+        Args:
+            record (Record): The record object to be added.
+        Raises:
+            TypeError: If the given object is not an instance of the Record class.
+        """
+        self[record.name.value] = record # отрабативает __setitem__ 
+    
+    def __getitem__(self, key: str) -> Record:
+        """
+        Retrieve a record from the address book by its name.
+
+        Args:
+            key (str): The name of the record to retrieve.
+        Returns:
+            Record: The record object corresponding to the given name.
+        Raises:
+            KeyError: If the provided name is not found in the address book.
+        """
+        record = self.data.get(key)
+        if record is None:
+            raise KeyError(f"This name {key} isn't in Address Book")
+        return record
+    
+    def __setitem__(self, key: str, val: Record) -> None:
+        """
+        Add or update a record in the address book.
+
+        Args:
+            key (str): The name of the record.
+            val (Record): The record object to be added or updated.
+        Raises:
+            TypeError: If the given value is not an instance of the Record class.
+            KeyError: If the provided name is already present in the address book.
+        """
+        if not isinstance(val, Record):
+            raise TypeError("Record must be an instance of the Record class.")
+        if key in self.data:
+            raise KeyError(f"This name '{key}' is already in contacts")
+        self.data[key] = val
+
+    def __delaitem__(self, key: str) -> None:
+        """
+        Delete a record from the address book by its name.
+
+        Args:
+            key (str): The name of the record to delete.
+        Raises:
+            KeyError: If the provided name is not found in the address book.
+        """
+        if not isinstance(key, str):
+            raise KeyError("Value must be string")
+        if key not in self.data:
+            raise KeyError(f"Can't delete contact {key} isn't in Address Book")
+        del self.data[key]
+
+    def to_dict(self) -> dict:
+        """
+        Convert the address book to a dictionary.
+
+        Returns:
+            dict: A dictionary representing the address book.
+        """
+        res_dict = {}
+        for record in self.data.values():
+            res_dict.update(record.to_dict())
+        return res_dict
+
+    def from_dict(self, data_json: dict) -> None:
+        """
+        Load data from a dictionary into the address book.
+
+        Args:
+            data_json (dict): A dictionary containing data for the address book.
+        Raises:
+            TypeError: If the provided data is not a dictionary.
+        """
+        if not isinstance(data_json, dict):
+            raise TypeError("this is not dict")
+        
+        for name, record in data_json.items():
+            self.add_record(
+                Record(name=name, phones=record['phones'], birthday=record['birthday'])
+            )
+
+    def __str__(self) -> str:
+        return '\n'.join([str(r) for r in self.values()])
+    
+    
+    def search(self, search_word: str) -> list[Record]:
+        """
+        Search for records containing the given search word.
+
+        Args:
+            search_word (str): The word to search in the adress book.
+        
+        Returns:
+            list[Record] or []: list whith found records.
+        """
+        search_list = []
+        for record in self.data.values():
+            str_val_record = f"{record.name} {' '.join([str(ph)for ph in record.phones])} {record.birthday}"
+            if search_word.lower() in str_val_record.lower():
+                search_list.append(record)       
+        return search_list 
+
+
+    def iterator(self, item_number: int) -> t.Generator[Record, int, None]:
+        """
+        Iterate through the records in the address book and yield groups of records.
+
+        Args:
+            item_number (int) > 0: The number of records to be yielded at a time.
+        
+        Yields:
+            List[Record]: A list containing a group of records.
+        
+        Notes:
+            If the given item_number is greater than the total number of records in the address book,
+            all records will be yielded in one group.
+
+        """
+        if item_number <= 0:
+            raise ValueError("Item number must be greater than 0.")
+        elif item_number > len(self.data): # если количство виводов(за раз) больше чем количество записей
+            item_number = len(self.data) # виводим все
+        
+        list_records = []
+        for counter, record in enumerate(self.data.values(), 1):
+            list_records.append(record)
+            if (not counter % item_number) or counter == len(self.data): 
+                yield list_records
+                list_records = []
+
+class AddressBookEncoder(json.JSONEncoder):
+    def default(self, obj: AddressBook | Record) -> dict[str, str | list[str]] | t.Any:
+        if isinstance(obj, (AddressBook, Record)):
+            return obj.to_dict()
+        return super().default(obj)
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+   
+
+
+
+
+
---
+++ package/contacts/contacts_handlers.py
@@ -0,0 +1,345 @@
+from functools import wraps
+import re, json
+from pathlib import Path
+
+from address_book import AddressBook, Record, Phone, AddressBookEncoder
+
+
+data_path = Path.home() / "data"
+data_path.mkdir(exist_ok=True)
+
+file_json  = data_path / "contacts.json"
+a_book = AddressBook() 
+try:
+    with open(file_json, "r") as file:
+        unpacked = json.load(file)
+    a_book.from_dict(unpacked)
+except FileNotFoundError:
+    with open(file_json, "w") as file:
+        json.dump({}, file)
+     
+def input_error(func):
+    @wraps(func) #для отображения доки/имени
+    def wrapper(*args):
+        """
+        декоратор ловит ошибки функций 
+        недостаток аргументов и созданние ошибки
+        затем возвращает на принт 
+        """
+        try:
+            return func(*args)
+        except IndexError:
+            command, formula = COMMANDS_HELP.get(func.__name__)
+            command = '/'.join(command)
+            return (f"Wrong input for this command [{command}]\n"
+                    f"for example:\t[{command}] {formula}"
+                    )
+        except ValueError as err:
+            return f"Input error: {str(err)}."
+        except KeyError as err:
+            return f"Give me the name from phonebook: {str(err)}."
+    return wrapper  
+
+@input_error
+def add_handler(data: list[str]) -> str:
+    """
+    Add a new contact to the address book.
+
+    Args:
+        data (list): A list containing contact information.
+
+    Returns:
+        str: A confirmation message for the added contact.
+    """
+    if len(data) <= 1 : raise IndexError
+    if len(data) >= 3:
+        name, phone, birthday = data
+        record = Record(name, [phone], birthday)
+    else:
+        name, phone, = data
+        record = Record(name, [phone])     
+
+    a_book.add_record(record)
+    return f"contact {str(record)} has be added"
+
+@input_error
+def add_handler_phone(data : list[str]) -> str:
+    """
+    Add a new phone number to an existing contact.
+
+    Args:
+        data (list): A list containing contact information.
+
+    Returns:
+        str: A confirmation message for the added phone number.
+    """
+    if len(data) <= 1 : raise IndexError
+    name, new_phone, = data
+    a_book[name].add_phone(new_phone)
+    return f"Successful added phone {Phone(new_phone)} to contact {name}"
+
+@input_error
+def change_handler_phone(data: list[str]) -> str:
+    """
+    Change the phone number of an existing contact.
+
+    Args:
+        data (list): A list containing contact information.
+
+    Returns:
+        str: A confirmation message for the changed phone number.
+    """
+    if len(data) <= 2 : raise IndexError
+    name, old_phone, new_phone, = data
+    a_book[name].change_phone(old_phone, new_phone)
+    return f"contact {name} has be changed phone to {Phone(new_phone)}"
+
+@input_error
+def del_handler_phone(data: list[str]) -> str:
+    """
+    Delete a phone number from an existing contact.
+
+    Args:
+        data (list): A list containing contact information.
+
+    Returns:
+        str: A confirmation message for the deleted phone number.
+    """
+    if len(data) <= 1 : raise IndexError
+    name, old_phone, = data
+    a_book[name].remove_phone(old_phone)
+    return f"phone - {Phone(old_phone)} from contact {name} has be deleted"
+
+@input_error
+def delete_handler(data: list[str]) -> str:
+    """
+    Delete an existing contact.
+
+    Args:
+        data (list): A list containing contact information.
+
+    Returns:
+        str: A confirmation message for the deleted contact.
+    """
+    if len(data) < 1 : raise IndexError
+    name, = data
+    del a_book[name]
+    return f"contact {name} has be deleted"
+
+@input_error
+def add_handler_birthday(data: list[str]) -> str:
+    """
+    Add a birthday to an existing contact.
+
+    Args:
+        data (list): A list containing contact information.
+
+    Returns:
+        str: A confirmation message for the added birthday.
+    """
+    if len(data) <= 1 : raise IndexError
+    name, birthday, = data
+    record = a_book[name]
+    if record.birthday is not None:
+        return f"this contact {name} is already have a date of birth: {record.birthday}"
+    record.change_birthday(birthday)
+    return f"contact {name} is added a date of birth: {record.birthday}"
+    
+@input_error
+def change_handler_birthday(data: list[str]) -> str:
+    """
+    Change the birthday of an existing contact.
+
+    Args:
+        data (list): A list containing contact information.
+
+    Returns:
+        str: A confirmation message for the changed birthday.
+    """
+    if len(data) <= 1 : raise IndexError
+    name, birthday, = data
+    a_book[name].change_birthday(birthday)
+    return f"contact {name} is changed to date of birth: {birthday}"  
+
+@input_error
+def handler_days_to_birthday(data: list[str]) -> str:
+    """
+    Get the number of days until the next birthday of an existing contact.
+
+    Args:
+        data (list): A list containing contact information.
+
+    Returns:
+        str: The number of days until the next birthday.
+    """
+    if len(data) < 1 : raise IndexError
+    name, = data
+    days = a_book[name].days_to_birthday() 
+    return f"{days} days left until {name}'s birthday"  
+
+@input_error
+def search_handler(data: list[str]) -> str:
+    """
+    Search for contacts by a given keyword.
+
+    Args:
+        data (list): A list containing search keyword.
+
+    Returns:
+        str: A formatted list of contacts matching the search keyword.
+    """
+    if len(data) < 1 : raise IndexError
+    search_word, = data
+    res = "\n".join([str(rec)[9:] for rec in a_book.search(search_word)])
+    if not res:  
+        return "not found any contact"
+    return res
+
+@input_error
+def show_page(data: list[str]) -> str:
+    """
+    Display contacts page by page.
+
+    Args:
+        data (list): A list containing the number of records per page.
+
+    Yields:
+        str: Formatted contacts for display, separated by pages.
+    """
+    count_record, = data if len(data) >= 1 else "1"
+    try: 
+        count_record = int(count_record)
+        yield "input any for next page"
+        for i, page in enumerate(a_book.iterator(count_record), 1):
+            page = "\n".join(map(lambda x: str(x)[9:], page ))
+            input("") 
+            head = f'{"-" * 15} Page {i} {"-" * 15}\n'
+            yield head + page
+        yield f'{"-" * 15} end {"-" * 15}\n'   
+    except ValueError: # без єтого гавнокода все падает(с вводом не цифр) 
+        for _ in range(1):
+            yield "invalid input count page"
+
+def help_handler(*args) -> str:
+   return "\n".join(
+        [
+        '{:<26}{:<}'.format(" / ".join(com_anot[0]), com_anot[1]) 
+        for com_anot in COMMANDS_HELP.values()
+        ]
+        )
+
+def show_all(*args) -> str:
+    # тут может бить красивая формат обертка через цикл и поля рекорда
+    return "\n".join([str(record)[9:] for record in a_book.values()])
+
+def hello_handler(*args) -> str:
+    return "How can I help you?"
+
+def exit_handler(*args) -> str:
+    with open(file_json, "w") as file:
+        json.dump(a_book, file, cls=AddressBookEncoder, sort_keys=True, indent=4)
+    return "Good bye!"
+
+def unknown_command(*args) -> str:
+    return 'Unknown command'
+
+def command_parser(row_str: str):
+    """
+    Parse a row string to identify and extract a command and its arguments.
+
+    Args:
+        row_str (str): A string containing the command and its arguments.
+
+    Returns:
+        tuple: A tuple containing the identified command key and a list of arguments.
+    """
+    row_str = re.sub(r'\s+', ' ', row_str) 
+    elements = row_str.strip().split(" ")
+    for key, value in BOT_COMMANDS.items():
+        if elements[0].lower() in value[0]:
+            return key, elements[1:]
+        elif " ".join(elements[:2]).lower() in value[0]: 
+            return key, elements[2:] 
+    return unknown_command, None
+
+BOT_COMMANDS = {
+    # при командах (с одинаковими первими словами)"add" & "add phone" работает какую первую найдет
+    hello_handler: (
+        ["hello"],
+        "hello"
+        ),
+    add_handler: (
+        ["add", "+"], 
+        "name phone(num) or name phone(num) date(ISO)"
+        ),
+    add_handler_phone: (
+        ["add_phone"], 
+        "name phone(num)"
+        ),
+    change_handler_phone: (
+        ["change phone"], 
+        "name old_phone(num) new_phone(num)"
+        ),
+    add_handler_birthday : (
+        ["birthday"], 
+        "name date(ISO)"
+        ),
+    change_handler_birthday: (
+        ["change birthday"], 
+        "name new_date(ISO)"
+        ),
+    handler_days_to_birthday: (
+        ["days"], 
+        "name"),
+    del_handler_phone: (
+        ["del phone"], 
+        "name phone(num)"
+        ),
+    delete_handler: (
+        ["delete"], 
+        "name"),
+    search_handler: (
+        ["search"], 
+        "search(alpha/num)"
+        ),
+    show_all: (
+        ["show all"], 
+        "show all address book"
+        ),
+    show_page : (
+        ["show page"], 
+        "int_num(positive) - show all address book"
+        ),
+    help_handler: (
+        ["help"], 
+        "- show all bot commands"
+        ),    
+    exit_handler: (
+        ["good bye", "close", "exit"], 
+        "- save changesets and exit"
+        ),
+}
+
+COMMANDS_HELP = {k.__name__:v for k,v in BOT_COMMANDS.items()}
+
+def main_contacts():
+    while True:
+        user_input = input(">>>")
+        if not user_input or user_input.isspace():
+            continue
+
+        func_handler, data = command_parser(user_input)
+        
+        if func_handler == show_page:
+            for page in func_handler(data):
+                print(page)
+            continue    
+
+        bot_message = func_handler(data)    
+        print(bot_message)
+        
+        if func_handler == exit_handler:
+            break
+        
+if __name__ == "__main__":
+    main_contacts()
