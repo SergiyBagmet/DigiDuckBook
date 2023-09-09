2c2c6a450844d3238876ef1e76a90c432188bb2f
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-09-09 20:31:49 +0300

    bot menu func name

--- package/main_bot.py
+++ package/main_bot.py
@@ -3,15 +3,23 @@ import typing as t
 
 
 
-def bot_unk() -> None:
-    print('Unknown command')
 
-def bot_exit() -> None:
-    print("Good bye!")
+def bot_unk() -> str:
+    return'Unknown command'
+
+def bot_help() -> str:
+    pass
+
+def bot_exit() -> str:
+    return "Good bye!"
 
 COMMANDS_DIGI_DUCK : dict[t.Callable, list[str] ] = {
     main_contacts : ["1", "ab", "contacts", "adress book"],
+    # main_notes : ["2", "notes", "note book"],
+    # main_sort : ["3","sort","folder", "dir"],
+    bot_help : ["help", "?", "start"],
     bot_exit : ["good bye", "close", "exit"],
+    
 }
 
 def bot_cm_parser(input_str :str) -> t.Callable | None:
@@ -31,10 +39,15 @@ def main_digi_duck() -> None:
             continue
        
         module_func = bot_cm_parser(user_input)
+
+        if module_func.__name__.startswith("bot"):
+            print(module_func())
+            if module_func == bot_exit:
+                break
+            continue
+
         module_func()
 
-        if module_func == bot_exit:
-            break
         
 
 if __name__ == "__main__":
