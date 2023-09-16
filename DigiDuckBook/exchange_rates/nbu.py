5fb06909af7b82d1dde9425d9a71ba2673182990
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-09-16 11:11:08 +0300

    OOP_nbu_short

--- DigiDuckBook/exchange_rates/nbu.py
+++ DigiDuckBook/exchange_rates/nbu.py
@@ -1,6 +1,30 @@
 import requests
+import typing as t
 
 
-nbu_response = requests.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
+url_nbu_rates_json = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+nbu_response = requests.get(url_nbu_rates_json)
 
-nbu_json = nbu_response.json()
+nbu_json: list[dict[str, str | int | float]]  = nbu_response.json()
+
+
+class NbuExchangeRates:
+    def __init__(self, full_data_nbu: t.Iterable[dict[str, str | int | float]]) -> None:
+        self.date_str = full_data_nbu[0]["exchangedate"]
+        self.grn_rate = self._short_data_nbu(full_data_nbu, ["USD", "EUR",])
+
+    def _short_data_nbu(
+            self, 
+            full_data_nbu: t.Iterable[dict[str, str | int | float]], 
+            country_keys: list[str]
+            ) -> list[dict[str, str | float]]:
+        
+        filter_data_nbu = filter(lambda x: x["cc"] in country_keys, full_data_nbu)
+        short_data_nbu = [
+                        {k: v for k, v in full_dict.items() if k in ["txt", "rate", "cc"]} 
+                        for full_dict in filter_data_nbu
+                        ]
+        return short_data_nbu
+    
+# grn_nbu = NbuExchangeRates(nbu_json)
+# print(grn_nbu.grn_rate)    
