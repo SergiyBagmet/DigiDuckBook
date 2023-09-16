dff47d2eec6278591bb89824a33774177bbcf3d0
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-09-16 22:06:59 +0300

    convert_money_code_flag

--- DigiDuckBook/exchange_rates/nbu.py
+++ DigiDuckBook/exchange_rates/nbu.py
@@ -7,30 +7,53 @@ nbu_response = requests.get(url_nbu_rates_json)
 
 nbu_json: list[dict[str, str | int | float]]  = nbu_response.json()
 
+COUNTRY_CODES = ["USD", "EUR", "PLN", "GBP", "CZK"]
 
 class NbuExchangeRates:
-    def __init__(self, full_data_nbu: t.Iterable[dict[str, str | int | float]]) -> None:
-        self.date_str = full_data_nbu[0]["exchangedate"]
-        self.grn_rate = self._short_data_nbu(full_data_nbu, ["USD", "EUR",])
+    def __init__(self, 
+                 full_data_nbu: t.Iterable[dict[str, str | int | float]], 
+                 codes: list[str]) -> None:
+        
+        self.date_str: str = full_data_nbu[0]["exchangedate"]
+        self.codes: list[str] = codes
+        self.short_data_nbu: dict[str, dict[str, str | float]] = self._short_data_nbu(full_data_nbu)
 
     def _short_data_nbu(
             self, 
             full_data_nbu: t.Iterable[dict[str, str | int | float]], 
-            country_keys: list[str]
-            ) -> list[dict[str, str | float]]:
+            ) -> dict[str, dict[str, str | float]]:
         
-        filter_data_nbu = filter(lambda x: x["cc"] in country_keys, full_data_nbu)
-        short_data_nbu = [
-                        {k: v for k, v in full_dict.items() if k in ["txt", "rate", "cc"]} | {"unrate": 1  /  full_dict["rate"] }
+        filter_data_nbu = filter(lambda x: x["cc"] in self.codes, full_data_nbu)
+        short_data_nbu = {full_dict["cc"]:
+                        {k: v for k, v in full_dict.items() if k in ["txt", "rate", "cc"]} 
+                        | {"unrate": 1.0 / full_dict["rate"] }
                         for full_dict in filter_data_nbu
-                        ]
+                        }
         return short_data_nbu
     
-    def __str__(el: dict[str, str | float]) -> str:
-        return tabulate(grn_nbu.grn_rate, 
+    def __str__(self) -> str:
+        return tabulate(self.short_data_nbu.values(), 
                         headers={"txt":"Country", "cc":"code", "rate":"rate", "unrate": "unrate"} ,
                         tablefmt='fancy_grid')
     
-grn_nbu = NbuExchangeRates(nbu_json)
+    def _validation_code(self, code: str) -> str.upper:
+        if (code:=code.upper()) not in self.codes:
+            raise KeyError(f"this code {code} not recognized")
+        return code
+    
+    def convert(self, money: int, code:str, to_grn: bool=True) -> float:
+        rate = "rate" if to_grn else "unrate"
+        if money <= 0 :
+            return 0
+        code = self._validation_code(code)
+        return money / self.short_data_nbu[code][rate] 
+    
+
+grn_nbu = NbuExchangeRates(nbu_json, COUNTRY_CODES)
 
 print(grn_nbu)
+usd = grn_nbu.convert(1_000, "usd")
+print(f'1_000 grn = {usd} usd at {grn_nbu.date_str}')
+
+grn = grn_nbu.convert(1_000, "usd", to_grn=False)
+print(f'1_000 usd = {grn} grn at {grn_nbu.date_str}')
